41969232f2d4b5d039493901d70a2e80c665ad20
Your Name <elnuraliyev1@gmail.com>
2022-06-08 14:36:43 -0500

    update2

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import plotly.figure_factory as ff
 import plotly.express as px
 import cufflinks as cf
 import datetime
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 # App title
 st.markdown('''
 # Stock Price App
@@ -17,8 +19,8 @@ st.write('---')
 
 # Sidebar
 st.sidebar.subheader('Query parameters')
-start_date = st.sidebar.date_input("Start date", datetime.date(2019, 1, 1))
-end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
+start_date = st.sidebar.date_input("Start date", datetime.date(2010, 1, 1))
+end_date = st.sidebar.date_input("End date", datetime.date(2022, 5, 31))
 
 # Retrieving tickers data
 ticker_list = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/s-and-p-500-companies/master/data/constituents_symbols.txt')
@@ -32,7 +34,21 @@ tickerDf = tickerData.history(period='1d', start=start_date, end=end_date) #get 
 #tickerData = yf.Ticker(tickerSymbol)
 
 #tickerDf = tickerData.history(period = '1d', start = '2010-5-31', end ='2020-5-31')
-fig = px.line(tickerDf, y="Close")
-st.plotly_chart(fig)
-fig = px.line(tickerDf, y="Volume")
+
+
+
+fig = make_subplots(rows=1, cols=2)
+
+fig.add_trace(
+    go.Scatter(y=tickerDf['Close']),
+    row=1, col=1
+)
+
+fig.add_trace(
+    go.Scatter(y=tickerDf['Volume']),
+    row=1, col=2
+)
+
+fig.update_layout(height=600, width=800, title_text="Close Price and Volume")
+
 st.plotly_chart(fig)
